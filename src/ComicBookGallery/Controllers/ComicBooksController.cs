b0f6bf2b1e0802af476b1a44466282e9b5268e7f
Tomasz Szwarczynski <seed@me.com>
2016-07-31 16:25:41 +0200

    Updated detail action method to return ActionResult

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,9 +8,17 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "Hello from the comic books controller";
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return new RedirectResult("/");
+            }
+
+            return new ContentResult()
+            {
+                Content = "Hello from the comic books controller"
+            };
         }
     }
 }
